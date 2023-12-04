2dffdb3e0db4a20eeb20be10efda7e41b6fe298f
Ayush Chinmay <ayush.chinmaya@gmail.com>
2023-12-04 03:39:03 -0500

    Add files via upload

---
+++ Ayush_Arduino_Code_Template.ino
@@ -0,0 +1,52 @@
+/**
+ *  @file <filename>
+ *  @brief <project description>
+ *  
+ *  Author       : Ayush Chinmay
+ *  Date Created : DD MONTH YYYY
+ *  Date Modified: DD MONTH YYYY
+ *
+ *  NOTES:
+ *      -   <note>
+ * 
+ *  CHANGELOG:
+ *      - DD MONTH YYYY
+ *              - <modifications>
+ * 
+ * TODO:
+ *    - [X]   <task 1>
+ *    - [-]   ,task 2.
+*/
+
+/** ==========[ INCLUDES ]========== **/
+// <includes>
+
+
+/** ==========[ CONSTANTS ]========== **/
+// <constants>
+
+
+/** ==========[ VARIABLES ]========== **/
+// <variables>
+
+
+/* ==========[ SETUP ]========== */
+void setup() {
+
+}
+
+/* ==========[ LOOP ]========== */
+void loop() {
+
+}
+
+/* ==========[ FUNCITON ]========== */
+
+/** ==========[ <func_name> ]========== **
+ *  <func_descr>
+ *  @param <param>
+ *  @return <return>
+ */
+ int func(int n) {
+    return n;
+ }
